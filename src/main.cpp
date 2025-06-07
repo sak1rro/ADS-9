54d14559e3aa90d8550839e2b4054fc0e9f3d5c3
sak1rro <91954858+sak1rro@users.noreply.github.com>
2025-06-07 16:56:27 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 // Copyright 2022 NNTU-CS
 #include "tree.h"
+#include <iostream>
 
 int main() {
+  std::vector<char> in = { '1', '2', '3' };
+  PMTree tree(in);
+
+  std::vector<std::vector<char>> perms = getAllPerms(tree);
+  for (const auto& perm : perms) {
+    for (char c : perm) std::cout << c;
+    std::cout << "\n";
+  }
   return 0;
 }
